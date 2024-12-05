f5d02b6d3b4fe7bc2d4b8001cacd0ccc4d882de2
ttalampas <124741869+ttalampas@users.noreply.github.com>
2024-12-04 19:51:06 -0800

    message

---
+++ csc131Lab02/Hellocs131.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("313734845 - This line was added by Tim Talampas.");
+		System.out.println("219345872 - This line was added by Alec Ferguson.");
+		System.out.println("219532795 - This line was added by Emmanuel Rodriguez.");
+		System.out.println("303002453 - This line was added by Steve Navarro.");
+		System.out.println("302938766 - This line was added by Clara Maldondo.");
+		System.out.println("301239988 - This line was added by Nisha Joshi.");
+		System.out.println("302578493 - This line was added by Anthony Vue.");
+		System.out.println("303213987 - This line was added by Yohannes Teklemariam.");
+
+}
+
+}
